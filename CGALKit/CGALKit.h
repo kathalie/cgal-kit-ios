4947724c013f10e156d80392fc0b9514a8f7c9cb
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-11-19 15:52:38 +0100

    Initial commit

---
+++ CGALKit/CGALKit.h
@@ -0,0 +1,18 @@
+//
+//  CGALKit.h
+//  CGALKit
+//
+//  Created by Kathryn Verkhogliad on 19.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CGALKit.
+FOUNDATION_EXPORT double CGALKitVersionNumber;
+
+//! Project version string for CGALKit.
+FOUNDATION_EXPORT const unsigned char CGALKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGALKit/PublicHeader.h>
+
+
---
+++ Tests/CGALKitTests/CGALKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import CGALKit
+
+final class CGALKitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
